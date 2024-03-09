55fe5210d319d2806b1f97f38f2b057a7c41fe14
AmolvShriram <amolvshriram@gmail.com>
2024-03-09 22:49:29 +0530

    type
    type

---
+++ 02_typeOf.js
@@ -0,0 +1,60 @@
+
+
+// var bankName;
+// console.log("========= Step 1 =========");
+// console.log("bankName before initialization:", bankName);
+// // console.log("bankName", bankName);
+// bankName= SBIBank;
+// console.log("bankName after initialization:", bankName);
+// var typeofbankName= typeof bankName;
+// console.log("type of bankName", typeof bankName);
+console.log("=========================== Step 1 ============================");var bankName;
+console.log("bankName is before initialization:>", bankName);
+var typeofbankName = typeof bankName;
+console.log(" Data type of bankName before initialization:>",typeofbankName);
+console.log("---------------------------------------------------------------");
+bankName = "SBI bank";
+console.log("bankName is after initialization:>", bankName);
+var typeofbankName = typeof bankName;
+console.log(" Data type of bankName after initialization:>", typeofbankName);
+
+console.log("=========================== Step 2 ============================");
+var marks = '90%';
+console.log(" marks:>", marks);
+var typeofmarks = typeof marks;
+console.log("Data type of marks:>", typeofmarks);
+
+
+console.log("=========================== Step 3 ============================");
+var isWorking;
+console.log("isWorking before initialization:>", isWorking);
+var typeofisWorking = typeof isWorking;
+console.log(" Data type of isWorking before initialization:>",typeofisWorking);
+console.log("---------------------------------------------------------------");
+isWorking = true;
+var typeofisWorking = typeof isWorking;
+console.log("isWorking after initialization:>", isWorking);
+console.log(" Data type of isWorking after initialization:>",typeofisWorking);
+
+
+console.log("=========================== Step 4 ============================");
+var totalCount = "Hundred and Seven";
+console.log(" totalCount before updation:>",totalCount);
+var typeoftotalCount = typeof totalCount;
+console.log(" Data type of totalCount before updation:>",typeoftotalCount);
+console.log("---------------------------------------------------------------");
+var totalCount = 107;
+console.log(" totalCount after updation :>", totalCount);
+var typeoftotalCount = typeof totalCount;
+console.log(" Data type of totalCount after updation:>",typeoftotalCount);
+
+console.log("=========================== ***** ============================");
+
+
+
+
+
+
+
+
+
