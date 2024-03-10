393f889e088c2728e23e4508c8eff325e55156e1
AmolvShriram <amolvshriram@gmail.com>
2024-03-10 15:39:19 +0530

    Variable swap
    Variable swap

---
+++ 01_variableAssign.js
@@ -0,0 +1,102 @@
+var n1=100; n2=200;
+console.log("---------------- Values before swap --------------");
+console.log("n1:=",n1, "n2:=",n2);
+console.log("---------------- Values after swap ---------------");
+var temp=n1;
+n1 = n2;
+n2 = temp;
+console.log("n1:=", n1,"n2:=", n2);
+
+console.log("==================== Step 1 ======================");
+var myFullName;
+console.log("my full name before initialization :>", myFullName);
+console.log("--------------------------------------------------");
+myFullName= "Amol Shriram";
+console.log("my full name after initialization :>", myFullName);
+console.log("--------------------------------------------------");  
+myFullName="Amol V Shriram";
+console.log("my full name after updation :>", myFullName); 
+
+console.log("==================== Step 2 ======================");
+
+var myLove;
+myLove="I Love Js";
+console.log("My love is:>",myLove);
+
+console.log("==================== Step 3 ======================");
+var firstName = "Amol";
+console.log("First Name:>", firstName);
+console.log("-------------------------------------");
+var lastName = "Shriram";
+console.log("Last Name:>", lastName);
+console.log("-------------------------------------");
+var age = 25;
+console.log("Age:>", age);
+console.log("-------------------------------------");
+var address = "Pandharpur , Solapur , Maharashtra";
+console.log(" Address:>", address);
+console.log("-------------------------------------");
+var pincode = 413304;
+console.log(" Pincode :>", pincode);
+console.log("-------------------------------------");
+var gender = "Male";
+console.log(" Gender :>", gender);
+console.log("-------------------------------------");
+var married = "false";
+console.log(" Married :>", married);
+console.log("--------------*******----------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
