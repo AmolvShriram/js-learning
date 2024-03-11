c7b667ec26c4d94752e5c59c6cbe90f8309bc69e
AmolvShriram <amolvshriram@gmail.com>
2024-03-11 13:32:51 +0530

    Swap Function
    Swap Function

---
+++ 03_funAssign02.js
@@ -0,0 +1,15 @@
+
+console.log("======================*****====================******* =================*****==============================");
+
+function bankDetails( bankName, accountNum, location, pinCode ){
+    // console.log("Argument :", bankName, accountNum, location, pinCode  );
+    // console.log("-------------------------------------------------------------------------------------------------------");
+    console.log("  Bank Name :", bankName, "Account No.:", accountNum, "Bank Location :", location, "Pincode :", pinCode );
+    console.log("=======================================================================================================");
+
+}
+bankDetails( " CITI Bank ", 3456782346, " Pune ",  431202);
+bankDetails(" Axis Bank ", 7856782345, " Mumbai ", 441202);
+bankDetails(" HDFC Bank ", 8956782345, " Pune ", 631202, " Open ")
+
+console.log("======================*****====================******* =================*****==============================");
