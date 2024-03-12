3b23d5906d7d46615dbe6d7b86b963ec1b336e6c
AmolvShriram <amolvshriram@gmail.com>
2024-03-12 15:05:09 +0530

    String Basics
    String Basics

---
+++ 04_stringBasics.js
@@ -0,0 +1,22 @@
+
+console.log("================================ Step 1 ======================================");
+console.log(` My Dream Company is " ISRO "`);
+console.log("================================ Step 2 ======================================");
+
+var myHobby1 = "Playing Outdoor Games";
+var myHobby2 = "Travelling";
+var myHobby3 = "Solving Puzzles";
+console.log(`My Hobbies are :: ${ myHobby1}, ${myHobby2}, ${myHobby3}` );
+console.log("================================ Step 3 ======================================");
+// var total =` ${myHobby1.length}+${myHobby2.length}+${myHobby3.length};`
+var len1 = myHobby1.length;
+var len2 = myHobby2.length;
+var len3 = myHobby3.length;
+var len = len1+len2+len3;
+console.log(`My Hobbies lengths  are:: `);
+console.log(` Hobby1 = ${myHobby1.length}`);
+console.log(` Hobby2 = ${myHobby2.length}`);
+console.log(` Hobby3 = ${myHobby3.length}`);
+console.log(`Sum of the total number of characters available in My Hobbies are = ${len}`);
+
+console.log("=============================== ######## =====================================");
