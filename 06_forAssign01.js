fc03f343134b0b1784c5184b652425f521b9b522
AmolvShriram <amolvshriram@gmail.com>
2024-03-19 09:33:13 +0530

    For Loop Assign.
    For Loop Assign.

---
+++ 06_forAssign01.js
@@ -0,0 +1,45 @@
+
+console.log(`================== From 5 to 15 Numbers Increment ======================`);
+for (let index = 5; index <= 15 ; index++) {
+    console.log(index);
+    
+}
+
+console.log(`================== From 50 to 40 Numbers Decrement =====================`);
+for (let index = 50; index >= 40 ; index--) {
+    console.log(index);
+    
+}
+
+console.log(`======================== First 15 Odd Numbers ==========================`);
+for (let index = 1; index <= 30 ; index=index+2) {
+    console.log(index);
+    
+}
+console.log(`======================== First 10 Even Numbers =========================`);
+for (let index = 0; index <= 20 ; index=index+2) {
+    console.log(index);
+    
+}
+
+console.log(`================================ 5th Table =============================`);
+for (let index = 5; index <= 50 ; index=index+5) {
+    console.log(index);
+    
+}
+
+console.log(`============================== 10th Table ==============================`);
+for (let index = 10; index <= 100 ; index=index+10) {
+    console.log(index);
+    
+}
+
+console.log(`====================== 10 table in Reverse Order =======================`);
+for (let index = 100; index >= 10 ; index=index-10) {
+    console.log(index);
+    
+}
+
+console.log(`=========================  ##############  =============================`);
+
+
